8cb6ab5dde8fe319797341034b46db45ea9f2b70
EntropicBlackhole <82614490+EntropicBlackhole@users.noreply.github.com>
2023-04-06 17:12:07 -0500

    Finished board command

--- database/commands/board.js
+++ database/commands/board.js
@@ -1,13 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
-
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const fs = require('fs');
+const functions = require('../bot/functions');
 module.exports = {
-	name: "",
-	description: "",
+	name: "Board",
+	description: "Lets your view your current match's board",
 	usage: "None",
 	data: new SlashCommandBuilder()
 		.setName('board')
 		.setDescription('Shows your current board!'),
-	async execute({ interaction, client }) {
-		await interaction.deferReply();
+	async execute({ interaction }) {
+		const players = JSON.parse(fs.readFileSync('./database/misc/players.json'));
+		if (!players[interaction.user.id]) return interaction.reply('You don\'t have a match ongoing yet! Start one with /create')
+		let matchID = players[interaction.user.id];
+		let match = JSON.parse(fs.readFileSync(`./database/misc/${matchID}.json`));
+		let board = await functions.drawBoard(match.board);
+		return interaction.reply({ content: "This is your board!", files: [new AttachmentBuilder(board)] })
+
 	},
 };
